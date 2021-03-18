8fb0e54bb8044eab4d346d59fd9a32f4c459ae42
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-03-18 15:37:20 -0300

    Shotgun surgery - Arquivo.getId()

---
+++ src/main/java/ufc/quixada/npi/contest/util/GetArquivo.java
@@ -0,0 +1,9 @@
+package ufc.quixada.npi.contest.util;
+
+import ufc.quixada.npi.contest.model.Arquivo;
+
+public class GetArquivo {
+	public static Integer getId(Arquivo arquivo) {
+		return arquivo.getId();
+	}
+}
